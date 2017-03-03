b401a81b1a9f3c9a9631e74fc3bf6ccfe9c424f0
Guihao (Jarvis) Liang <guihaol@guihaol1.zillow.local>
2017-03-03 11:01:22 -0800

    Debian configuration v2.1

--- debian.sh
+++ debian.sh
@@ -10,6 +10,13 @@ confirm_install () {
 	fi
 }
 
+confirm_or_exit () {
+	read $ans
+	if [[ -n $ans && $ans != "y" && $ans != "Y" ]]; then
+		echo $1 terminated.	
+		exit 35
+	fi
+}
 apt_get_update_upgrade () {
 	sudo apt-get update
 	sudo apt-get upgrade
@@ -38,6 +45,9 @@ confirm_install "git"
 # zsh
 echo "chsh -s to change shell"
 confirm_install "zsh"
+
+# oh-my-zsh
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # zsh pkg manager
 confirm_install "zsh-antigen"
@@ -70,7 +80,7 @@ cd -
 
 # tmux
 echo "Installing tmux"
-echo "Adding jessie backports first [Y/N]"
+confirm_or_exit "Adding jessie backports first [Y/N]"
 apt_get_update_upgrade
 read ans
 if [[ $ans == "n" || $ans == "N" ]]; then
@@ -80,3 +90,16 @@ fi
 sudo apt-get -t jessie-backports install tmux
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 echo "Enter tmux and <prefix>+I to install plugins."
+
+
+# adding AMD/ATI drivers
+echo "\n\nTo make sure your display work well,
+find out the type of your graphic card first:
+$(lspci -nn | grep VGA)
+Then search on Debian Wiki to get more help.
+
+For my case, my Debian box uses ATI 7700.
+Add deb http://httpredir.debian.org/debian/ jessie main contrib non-free to /etc/apt/source.list.
+sudo apt-get update && sudo apt-get upgrade.
+apt-get install firmware-linux-nonfree libgl1-mesa-dri xserver-xorg-video-ati
+"
