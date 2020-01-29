598c5816406b2f5272e62f22caf86ace3f32b83b
guihao_liang <guihao_liang@apple.com>
2020-01-29 12:38:15 -0800

    create a shell install script for mac

---
+++ install_for_mac.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+set -eux -o pipefail
+
+##################### HOMEBREW ########################
+
+# utils
+
+function check_command_exists {
+  [ $# -eq 1 ] && $(command -v $1 2>&1 > /dev/null)
+}
+
+function check_if_installed {
+  [ $# -eq 1 ] && $(brew list $1 2>&1 > /dev/null)
+}
+
+function brew_install_if_not_exists {
+  for item in $@; do
+    if ! check_command_exists "$item" && \
+        ! check_if_installed "$item"; then
+      brew install $item
+    fi
+  done
+}
+
+#
+# install homebrew if not exist
+#
+
+if ! check_command_exists brew; then
+  /usr/bin/ruby -e "$(curl -fsSL \
+    https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+#
+# general purpose utils
+#
+
+brew_install_if_not_exists coreutils colordiff htop
+
+#
+# install C++ Utils
+#
+
+brew_install_if_not_exists clang-format
